75b9dde5e3b44b874528238e8c678dee810fadc9
Paul Peregud <paulperegud@gmail.com>
2018-08-21 16:55:57 +0200

    list tests

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,11 +1,13 @@
-import pytest
 import os
-
 from solc_simple import Builder
+
 
 def test_compilation_does_not_crash():
     OWN_DIR = os.path.dirname(os.path.realpath(__file__))
-    CONTRACTS_DIR = os.path.abspath(os.path.realpath(os.path.join(OWN_DIR, '../priv')))
-    OUTPUT_DIR = os.path.abspath(os.path.realpath(os.path.join(OWN_DIR, '../priv/build')))
-    builder = Builder(CONTRACTS_DIR, OUTPUT_DIR)
+
+    def path(relative_path):
+        joined = os.path.join(OWN_DIR, relative_path)
+        return os.path.abspath(os.path.realpath(joined))
+
+    builder = Builder(path('../priv'), path('../priv/build'))
     builder.compile_all()
