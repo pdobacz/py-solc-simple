5c2e6f4eb76dc9b31e6a3587eb5b33eb423251cf
Paul Peregud <paulperegud@gmail.com>
2018-08-21 16:32:25 +0200

    bump version to re-publish

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-solc-simple",
-    version="0.0.8",
+    version="0.0.9",
     author="Paul Peregud",
     author_email="paulperegud@gmail.com",
     description="Simple wrapper around py-solc. Needs solc binary in PATH",
